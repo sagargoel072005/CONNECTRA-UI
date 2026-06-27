da5ffc5a5c585d3ffb12d90baea8b4bddca37bd5
sagargoel072005 <goels4280@gmail.com>
2026-06-27 17:00:43 +0530

    base url changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,4 @@
-//export const BASE_URL =location.hostname == "localhost:3000" ? "http://localhost:1507" : "/api";
-
 export const BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:1507"
-    : "api";
+    : "/api";
